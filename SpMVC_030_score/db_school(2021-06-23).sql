5342f4e017adc743fb30a2874d48b992eee9b07c
hooninfinity <hooninfinity@naver.com>
2021-06-23 16:30:32 +0900

    mybatis

---
+++ SpMVC_030_score/db_school(2021-06-23).sql
@@ -0,0 +1,46 @@
+CREATE DATABASE db_school;
+USE db_school;
+CREATE TABLE tbl_student (
+	st_num	CHAR(8)		PRIMARY KEY,
+	st_name	VARCHAR(20)	NOT NULL,	
+	st_dept	VARCHAR(20)	NOT NULL,	
+	st_grade	INT	NOT NULL,	
+	st_tel	VARCHAR(15)	NOT NULL,
+	st_addr	VARCHAR(125)		
+);
+
+INSERT INTO tbl_student(st_num, st_name, st_dept, st_grade, st_tel)
+VALUES('2021001','홍길동','컴공과',3,'010-111-1234');
+COMMIT;
+
+DROP TABLE tbl_score;
+CREATE TABLE tbl_score(
+sc_seq	BIGINT	AUTO_INCREMENT	PRIMARY KEY,
+sc_stnum	CHAR(8)	NOT NULL,	
+sc_sbcode	CHAR(4)	NOT NULL,
+sc_score	INT	NOT NULL	
+);
+
+INSERT INTO tbl_score (sc_stnum, sc_sbcode, sc_score)
+VALUES('2021001','S001',90);
+INSERT INTO tbl_score (sc_stnum, sc_sbcode, sc_score)
+VALUES('2021001','S001',90);
+INSERT INTO tbl_score (sc_stnum, sc_sbcode, sc_score)
+VALUES('2021001','S001',90);
+
+CREATE TABLE tbl_subject(
+	sb_code	CHAR(4),	
+	sb_name	VARCHAR(20)	NOT NULL,
+	sb_prof	VARCHAR(20)	
+);
+
+INSERT INTO tbl_subject (sb_code, sb_name, sb_prof)
+VALUES('S001','국어','홍길동');
+INSERT INTO tbl_subject (sb_code, sb_name, sb_prof)
+VALUES('S002','영어','이몽룡');
+INSERT INTO tbl_subject (sb_code, sb_name, sb_prof)
+VALUES('S003','수학','성춘향');
+SELECT * FROM tbl_subject;
+DELETE FROM tbl_subject WHERE sb_code = 0001;
+SET SQL_SAFE_UPDATES = 0;
+
---
+++ SpMVC_030_score/src/main/java/com/callor/score/model/ScoreVO.java
@@ -0,0 +1,19 @@
+package com.callor.score.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ScoreVO {
+	private Long sc_seq; 	//	BIGINT
+	private String sc_stnum; 	//	CHAR(8)
+	private String sc_sbcode; //	CHAR(4)
+	private int sc_score; 	//	INT
+}
