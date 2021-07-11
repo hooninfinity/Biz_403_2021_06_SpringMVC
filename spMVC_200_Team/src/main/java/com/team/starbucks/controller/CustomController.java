0bcdecc82acea01f1fb7b60e4f7ebfc6be686d3d
hooninfinity <hooninfinity@naver.com>
2021-07-11 22:26:09 +0900

    team

--- spMVC_200_Team/src/main/java/com/team/starbucks/controller/CustomController.java
+++ spMVC_200_Team/src/main/java/com/team/starbucks/controller/CustomController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.team.starbucks.model.CategoryDTO;
 import com.team.starbucks.model.CustomDTO;
@@ -41,27 +42,56 @@ public class CustomController {
 
 		return "custom/list";
 	}
-//
-//	@RequestMapping(value = "/input", method = RequestMethod.GET)
-//	public String insert1() {
-//		return "custom/input";
-//	}
-//
-//	@RequestMapping(value = "/input/{url}", method = RequestMethod.POST)
-//	public String insert(@RequestParam("menukinds") int menu_kinds, Model model) {
-//
-//		List<CategoryDTO> menukindsList = cuService.findByMenukinds(menu_kinds);
-//		log.debug("munukindsList {}", menukindsList.toString());
-//		model.addAttribute("KINDS", menukindsList);
-//
-//		return "custom/input";
-//	}
+
 	@RequestMapping(value = "/input", method = RequestMethod.GET)
 	public String base1(Model model) {
 		List<CategoryDTO> menukindsList = cuService.findBybase1();
 		log.debug("menuKinds{}", menukindsList.toString());
 		model.addAttribute("BASE1", menukindsList);
 		return "custom/input";
-		
+
 	}
+
+	@RequestMapping(value = "/input/base1", method = RequestMethod.GET)
+	public String insert(@RequestParam("menukinds") int menu_kinds, Model model) {
+		List<CategoryDTO> menukindsList = cuService.findByMenukinds(menu_kinds);
+		log.debug("munukindsList {}", menukindsList.toString());
+		model.addAttribute("KINDS", menukindsList);
+		return "custom/base2";
+	}
+
+	@RequestMapping(value = "/insert", method = RequestMethod.GET)
+	public String saveMenu(Model model, @RequestParam("menucode") int menu_code) {
+		CategoryDTO cateDto = cuService.findByMenuName(menu_code);
+		log.debug(cateDto.toString());
+		model.addAttribute("CHOISEMENU", cateDto);
+		return "custom/input";
+	}
+
+	@RequestMapping(value = "/insert", method = RequestMethod.POST)
+	public int saveMenu(@RequestParam("menucode") int menu_code, CustomVO cuVO) {
+		cuVO.builder().menu_code(menu_code).menu_option(cuVO.getMenu_option()).menu_title(cuVO.getMenu_title())
+				.menu_img(cuVO.getMenu_img()).user_id(cuVO.getUser_id()).build();
+		log.debug(cuVO.toString());
+		return cuService.insert(cuVO);
+	}
+
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String dumy(Model model) {
+		List<CategoryDTO> allCate = cuService.findBybase1();
+		log.debug(" allCate {}", allCate.toString());
+		model.addAttribute("BASE1", allCate);
+//
+//		
+//		List<CategoryDTO> menukindsList = cuService.findByMenukinds(cateVO.getMenu_kinds());
+//		log.debug("munukindsList {}", menukindsList.toString());
+//		model.addAttribute("KINDS", menukindsList);
+//
+//		CategoryDTO cateDto = cuService.findByMenuName(cateVO.getMenu_code());
+//		log.debug(cateDto.toString());
+//		model.addAttribute("CHOISEMENU", cateDto);
+
+		return "custom/test";
+	}
+
 }
